77d241150d4762ee3aac01ac5bf024c7ec396fad
unknown <sjjoseph106@gmail.com>
2022-03-20 14:37:26 +0900

    Posting

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,4 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
+import { dbService } from "fbase";
 
-const Home = () => <span>Home</span>;
+const Home = () => {
+  const [post, setPost] = useState("");
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    await dbService.collection("posts").add({
+      post,
+      createdAt: Date.now(),
+    });
+    setPost("");
+  };
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setPost(value);
+  };
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          value={post}
+          onChange={onChange}
+          type="text"
+          placeholder="What's on your mind?"
+          maxLength={120}
+        />
+        <input type="submit" value="Post" />
+      </form>
+    </div>
+  );
+};
+
 export default Home;
