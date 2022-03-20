d77b71c2277e1b9d4b49dd931646b1300d7bf5fb
unknown <sjjoseph106@gmail.com>
2022-03-20 15:42:19 +0900

    Getting the Posts

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,8 +1,23 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { dbService } from "fbase";
 
 const Home = () => {
   const [post, setPost] = useState("");
+  const [posts, setPosts] = useState([]);
+  const getPosts = async () => {
+    const dbPosts = await dbService.collection("posts").get();
+    dbPosts.forEach((document) => {
+      const postObject = {
+        ...document.data(),
+        id: document.id, // map 함수를 위해 id를 임의로 추가
+      };
+      // 이전 값들 앞에 forEach로 들어오는 데이터 붙이기
+      setPosts((prev) => [postObject, ...prev]);
+    });
+  };
+  useEffect(() => {
+    getPosts();
+  }, []);
   const onSubmit = async (event) => {
     event.preventDefault();
     await dbService.collection("posts").add({
@@ -29,6 +44,13 @@ const Home = () => {
         />
         <input type="submit" value="Post" />
       </form>
+      <div>
+        {posts.map((post) => (
+          <div key={post.id}>
+            <h4>{post.post}</h4>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
